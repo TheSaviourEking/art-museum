09cba515f8828272cdfc928e80df5561bfbdadb4
TheSaviourEking <saviour.eking@outlook.com>
2024-05-08 03:14:51 +0100

    chore: add GalleryNavigation component and styling

--- src/components/GalleryNavigation/index.js
+++ src/components/GalleryNavigation/index.js
@@ -1,22 +1,21 @@
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import './GalleryNavigation.css';
-// let i = 0;
+
 const GalleryNavigation = ({ galleries }) => {
     return (
-        <nav>
-            <h1>Galleries</h1>
-            <ul>
-                <li><NavLink exact to='/'>home</NavLink></li>
+        <header>
+            <h1>GalleryNavigation</h1>
+            <nav>
+                <NavLink exact to='/'>home</NavLink>
                 {galleries.map(gallery => {
-                    const { id } = gallery;
+                    const { name, id } = gallery;
                     return (
-                        <li key={id}>
-                            <NavLink exact to={`/galleries/${id}`}>{id}</NavLink>
-                        </li>
+                        <NavLink key={id} to={`/galleries/${id}`}>{name}</NavLink>
                     )
                 })}
-            </ul>
-        </nav>
+            </nav>
+        </header>
     )
 }
+
 export default GalleryNavigation;
