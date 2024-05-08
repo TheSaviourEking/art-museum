8e94150981df9be22f79dacf5b3faddd3d8739d1
TheSaviourEking <saviour.eking@outlook.com>
2024-05-08 03:14:31 +0100

    chore: add GalleryView component and styling

--- src/components/GalleryView/index.js
+++ src/components/GalleryView/index.js
@@ -1,24 +1,33 @@
-import { Route, useParams } from "react-router-dom/cjs/react-router-dom";
+import { Route, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import ArtImageTile from "../ArtImageTile";
+
+import './GalleryView.css';
+
 import ArtDescription from "../ArtDescription";
 
 const GalleryView = ({ galleries }) => {
     const { galleryId } = useParams();
-    const { name, objects, url } = galleries.find(gallery => gallery.id === parseInt(galleryId))
-    const gallery = galleries.find(gallery => gallery.id === parseInt(galleryId))
-    // console.log(gallery)
+    const gallery = galleries.find(gallery => gallery.id === parseInt(galleryId));
+    const { name, objects } = gallery;
+
     return (
-        <>
+        <div>
             <h2>{name}</h2>
             <Route exact path='/galleries/:galleryId'>
-                {objects.map(object => {
-                    return <ArtImageTile key={object.id} art={object} galleryId={galleryId} />
-                })}
+                <div className="artImagePreview">
+                    {
+                        objects.map(artWork => {
+                            return (
+                                <ArtImageTile key={artWork.id} art={artWork} galleryId={galleryId} />
+                            )
+                        })
+                    }
+                </div>
             </Route>
-            <Route exact path='/galleries/:galleryId/art/:artId'>
-                <ArtDescription url={url} gallery={gallery} />
+            <Route path='/galleries/:galleryId/art/:artId'>
+                <ArtDescription gallery={gallery} />
             </Route>
-        </>
+        </div>
     )
 }
 
